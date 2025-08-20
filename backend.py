d1eb1abe115ce0b2d9d0d7c9b703b4fd7fa1e525
MADEISAH <maryamassadeeqahisah@gmail.com>
2025-08-20 16:59:25 +0100

    second commit

--- backend.py
+++ backend.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/planA')
+def planA():
+    return render_template('planA.html')
 
 
 if __name__ == '__main__':
